7a1dd6cd4e4cf0a82f1bcfd23c907fa3579ff74f
Jafaar0 <h.hayma@orange.fr>
2024-09-24 15:20:10 +0200

    question 1

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
+    for (int i=2; i<=10; i+=2) {
+        std::cout << i << std::endl;
+    }
     std::cout << "Hello, World!" << std::endl;
+
+    for (int i=9; i>=0; i-=2) {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
