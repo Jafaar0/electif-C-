97f8757a611178adf8b60f19d6dc7dd32a9070a0
Jafaar0 <h.hayma@orange.fr>
2024-09-24 15:30:19 +0200

    fin exo 1

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
-int main() {
-    for (int i=2; i<=10; i+=2) {
-        std::cout << i << std::endl;
+void printGreetings(int bound) {
+
+    for (int i=1; i<=bound; i+=1) {
+        if(i%2==0) {
+            std::cout << i << std::endl;
+        }
     }
     std::cout << "Hello, World!" << std::endl;
 
-    for (int i=9; i>=0; i-=2) {
-        std::cout << i << std::endl;
+    for (int i=bound; i>=0; i-=1) {
+        if(i%2==1) {
+            std::cout << i << std::endl;
+        }
     }
+}
+
+int main() {
+    printGreetings(-10);
     return 0;
 }
